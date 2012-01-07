183efbfa87656dab21fe1e70cd038feb151065a0
Jorge Álvarez <coke.alvaradez@gmail.com>
2012-01-07 10:42:24 -0300

    versión 0.1.1, una var por scope y agregada versión min

--- jquery.simpleTabs.js
+++ jquery.simpleTabs.js
@@ -1,5 +1,5 @@
 /* 
- * simpleTabs jQuery Plugin v0.1.0
+ * simpleTabs jQuery Plugin v0.1.1
  * Copyright (c) 2012 Jorge Álvarez
  *
  * Licensed under the MIT license
@@ -10,11 +10,10 @@
     
     var settings = $.extend( {
       select: 1
-    }, options);
-    
-    var $this = $(this),
-        $tabs = $this.children('div.tab'),
-        $nav = $this.find('ul.nav');
+    }, options),
+    $this = $(this),
+    $tabs = $this.children('div.tab'),
+    $nav = $this.find('ul.nav');
     
     $nav.find('a').click(function(e){
       var href = $(this).attr('href');
